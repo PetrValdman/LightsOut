415a3b4674e681c4888f44cdc404d4d910842d23
Petr <petr.valdman@educanet.cz>
2021-02-08 02:57:47 +0000

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LightsOut';
+  grid = [
+           [false, true, false],
+           [false, true, false],
+           [false, true, false]
+  ];
+  lengthOfGrid = 4;
+
+  Lightswitch(columnInd: number,cellInd: number): void{
+console.log(columnInd, cellInd);
+if(columnInd -1 > -1)
+this.grid[columnInd -1][cellInd]=!this.grid[columnInd- 1][cellInd];
+if(cellInd -1 > -1)
+this.grid[columnInd][cellInd - 1]=!this.grid[columnInd][cellInd - 1];
+if(cellInd +1 < 3)
+this.grid[columnInd][cellInd + 1]=!this.grid[columnInd][cellInd + 1];
+if(columnInd +1 < 3)
+this.grid[columnInd + 1][cellInd]=!this.grid[columnInd + 1][cellInd];
+this.grid[columnInd][cellInd]=!this.grid[columnInd][cellInd];
+  }
+
+  Won(): boolean{
+   for(let i=0; i< this.grid.length; i++){
+    for(let o=0; o< this.grid[i].length; o++){
+    if(this.grid[i][o] == false)
+    return false;
+    }
+    }
+    return true;
+    }
+
 }
+  // ngOnInit(): void {
+  //    for (let i= 0; i < this.router.config.length; i++) {
+  //      if(this.router.config[i].path != undefined){
+  //        this.routes.push(this.router.config[i].path);
+  //     }
+  //    }
+  //  }
+
+
