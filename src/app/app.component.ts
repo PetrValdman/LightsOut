5ec821290abe81c4eefaf15b280f176bef918eee
Petr <petr.valdman@educanet.cz>
2021-02-14 19:32:26 +0000

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,11 @@ export class AppComponent {
            [false, true, false],
            [false, true, false]
   ];
+  turnstaken = 0;
+  leastTurnsTaken = 0;
   lengthOfGrid = 4;
 
-  Lightswitch(columnInd: number,cellInd: number): void{
+  lightswitch(columnInd: number,cellInd: number): void{
 console.log(columnInd, cellInd);
 if(columnInd -1 > -1)
 this.grid[columnInd -1][cellInd]=!this.grid[columnInd- 1][cellInd];
@@ -25,18 +27,47 @@ this.grid[columnInd][cellInd + 1]=!this.grid[columnInd][cellInd + 1];
 if(columnInd +1 < 3)
 this.grid[columnInd + 1][cellInd]=!this.grid[columnInd + 1][cellInd];
 this.grid[columnInd][cellInd]=!this.grid[columnInd][cellInd];
-  }
+this.turncounter();
+console.log(this.turnstaken);
+this.won();
 
-  Won(): boolean{
+  }
+  randomGrid(): void{
+  this.turnstaken = 0;
+   for(let i=0; i< this.grid.length; i++){
+      for(let o=0; o< this.grid[i].length; o++){
+        let c = Math.random();
+        if(c < 0.5)
+        this.grid[i][o] = false;
+        else
+        this.grid[i][o] = true;
+    }
+    }
+    }
+  turncounter(){
+  this.turnstaken++;
+}
+  bestScore(){
+  if(this.turnstaken < this.leastTurnsTaken || this.leastTurnsTaken == 0)
+  this.leastTurnsTaken = this.turnstaken;
+  }
+  won(): boolean{
    for(let i=0; i< this.grid.length; i++){
     for(let o=0; o< this.grid[i].length; o++){
-    if(this.grid[i][o] == false)
+    if(this.grid[i][o] == false){
     return false;
+    break;
     }
     }
+    }
+    console.log("won");
+    this.bestScore();
+    this.randomGrid();
     return true;
     }
-
+ngOnInit(): void {
+this.randomGrid();
+     }
 }
   // ngOnInit(): void {
   //    for (let i= 0; i < this.router.config.length; i++) {
